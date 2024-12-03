b9957abc5da89b10c2cf20717475d6aef3421873
Sbenedict97 <114118891+Sbenedict97@users.noreply.github.com>
2024-12-03 07:47:00 -0700

    Update subscription.js

--- models/subscription.js
+++ models/subscription.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
-const categorySchema = new mongoose.Schema({
+const subscriptionSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  price: { type: Number, required: true },
+  duration: { type: String, required: true }, // Example: "Monthly", "Yearly"
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
   description: { type: String, required: true },
+  isActive: { type: Boolean, default: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Category', categorySchema);
+module.exports = mongoose.model('Subscription', subscriptionSchema);
