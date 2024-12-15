541c3a592cb241ea5aab8b7113c467c68633c058
Sbenedict97 <114118891+Sbenedict97@users.noreply.github.com>
2024-12-15 12:27:13 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1);
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
   }
 };
 
-connectDB();
-
-module.exports = mongoose;
+module.exports = connectDB;
